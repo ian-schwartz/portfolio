365e2789b5e2d6e659c5acb57313776984fe1fb8
Ian Schwartz <ian.schwartz23@gmail.com>
2020-06-24 17:03:35 -0700

    implemented contact form

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="app">
       <Route exact path="/" component={Home}/>
       <Route path="/about" component={About}/>
+      <Route path="/contact" component={Contact} />
       <Route path="/projects" component={Projects}/>
     </div>
   );
---
+++ src/components/Contact.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Nav from "./Nav";
+import { Button, OutlinedInput } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  OutlinedInput: {
+    margin: "1.5rem",
+    width: "25%",
+    [theme.breakpoints.down("sm")]: { width: "50%" },
+  },
+}));
+
+const Contact = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Nav />
+      <h1 className="heading">CONTACT ME.</h1>
+      <div className="project-text">
+        <h2>Drop me a line and I'll respond as soon as possible!</h2>
+      </div>
+      <form className="contact" action="POST" data-netlify="true">
+        <OutlinedInput
+          name="name"
+          placeholder="Name"
+          required={true}
+          className={classes.OutlinedInput}
+        ></OutlinedInput>
+        <OutlinedInput
+          name="email"
+          placeholder="Your Email Address"
+          required={true}
+          className={classes.OutlinedInput}
+        ></OutlinedInput>
+        <OutlinedInput
+          name="message"
+          placeholder="Message"
+          required={true}
+          className={classes.OutlinedInput}
+          multiline={true}
+          rows={5}
+        ></OutlinedInput>
+        <div data-netlify-recaptcha="true"></div>
+        <Button
+          variant="contained"
+          color="primary"
+          type="sumbit"
+          style={{ margin: "1.5rem" }}
+        >
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
