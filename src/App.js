b0294572a47a6c42ea754d8d0c1ecfd3194b38a3
Ian Schwartz <ian.schwartz23@gmail.com>
2020-07-20 12:48:15 -0700

    basic ga setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import './App.css';
-import 'react-typist/dist/Typist.css';
-import { Route } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import React, { useEffect } from "react";
+import "./App.css";
+import "react-typist/dist/Typist.css";
+import { Route } from "react-router-dom";
+import ReactGA from "react-ga";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("UA-173105216-1");
+    // Report Page View
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div className="app">
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
       <Route path="/contact" component={Contact} />
-      <Route path="/projects" component={Projects}/>
+      <Route path="/projects" component={Projects} />
     </div>
   );
 }
