9902e08082aeaffb45bcdf497a37e57f97292f2e
Ian Schwartz <ian.schwartz23@gmail.com>
2020-11-11 01:30:05 -0800

    responsive nav menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
@@ -26,8 +27,9 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Nav = () => {
-  const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState(null);
+  const classes = useStyles();
+  const menuBreakpoint = useMediaQuery("(max-width:768px)");
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -39,38 +41,78 @@ const Nav = () => {
 
   return (
     <>
-      <AppBar className={classes.appBar}>
-        {/* <Link to='/' className={classes.link}>
-          <Button className={classes.button} id='home-btn'>
-            Home
-          </Button>
-        </Link> */}
-
-        <Toolbar className='nav-container'>
-        <Link to='/'>
-            <CodeIcon color='primary' fontSize='large' />
-          </Link>
-          <Button
-            aria-controls='simple-menu'
-            aria-haspopup='true'
-            onClick={handleClick}
-          >
-            <MenuIcon color='primary' fontSize='large' />
-          </Button>
-          <Menu
-            id='simple-menu'
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>
-              {" "}
+      {menuBreakpoint ? (
+        <AppBar className={classes.appBar}>
+          <Toolbar className='nav-container'>
+            <Link to='/'>
+              <CodeIcon color='primary' fontSize='large' />
+            </Link>
+            <Button
+              aria-controls='simple-menu'
+              aria-haspopup='true'
+              onClick={handleClick}
+            >
+              <MenuIcon color='primary' fontSize='large' />
+            </Button>
+            <Menu
+              id='simple-menu'
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>
+                {" "}
+                <Link to='/about' className={classes.link}>
+                  <Button className={classes.button}>About</Button>
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a
+                  href='https://resume.creddle.io/resume/bbjuttd019o'
+                  className={classes.link}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <span>
+                    <Button className={classes.button}>Resume</Button>
+                  </span>
+                </a>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <Link to='/contact' className={classes.link}>
+                  <Button className={classes.button}>Contact</Button>
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <Link to='/projects' className={classes.link}>
+                  <Button
+                    color='primary'
+                    variant='outlined'
+                    className={classes.button}
+                  >
+                    Projects
+                  </Button>
+                </Link>
+              </MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+      ) : (
+        <AppBar className={classes.appBar}>
+          <Toolbar className='nav-container'>
+            <Link to='/'>
+              <CodeIcon color='primary' fontSize='large' />
+            </Link>
+            <div className='nav-links'>
+              <Link to='/' className={classes.link}>
+                <Button className={classes.button} id='home-btn'>
+                  Home
+                </Button>
+              </Link>
               <Link to='/about' className={classes.link}>
                 <Button className={classes.button}>About</Button>
               </Link>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
               <a
                 href='https://resume.creddle.io/resume/bbjuttd019o'
                 className={classes.link}
@@ -81,13 +123,9 @@ const Nav = () => {
                   <Button className={classes.button}>Resume</Button>
                 </span>
               </a>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
               <Link to='/contact' className={classes.link}>
                 <Button className={classes.button}>Contact</Button>
               </Link>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
               <Link to='/projects' className={classes.link}>
                 <Button
                   color='primary'
@@ -97,10 +135,10 @@ const Nav = () => {
                   Projects
                 </Button>
               </Link>
-            </MenuItem>
-          </Menu>
-        </Toolbar>
-      </AppBar>
+            </div>
+          </Toolbar>
+        </AppBar>
+      )}
     </>
   );
 };
