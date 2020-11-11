b6a452abf7d4aff6d9e14c09a58c30644e5c44d7
Ian Schwartz <ian.schwartz23@gmail.com>
2020-11-11 01:02:31 -0800

    added menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 import CodeIcon from "@material-ui/icons/Code";
+import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles((theme) => ({
   link: {
@@ -24,45 +27,78 @@ const useStyles = makeStyles((theme) => ({
 
 const Nav = () => {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       <AppBar className={classes.appBar}>
-        <Toolbar className="nav-container">
-          <Link to="/">
-            <CodeIcon color="primary" fontSize="large" />
+        {/* <Link to='/' className={classes.link}>
+          <Button className={classes.button} id='home-btn'>
+            Home
+          </Button>
+        </Link> */}
+
+        <Toolbar className='nav-container'>
+        <Link to='/'>
+            <CodeIcon color='primary' fontSize='large' />
           </Link>
-          <div className="nav-links">
-            <Link to="/" className={classes.link}>
-              <Button className={classes.button} id="home-btn">
-                Home
-              </Button>
-            </Link>
-            <Link to="/about" className={classes.link}>
-              <Button className={classes.button}>About</Button>
-            </Link>
-            <a
-              href="https://resume.creddle.io/resume/bbjuttd019o"
-              className={classes.link}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span>
-                <Button className={classes.button}>Resume</Button>
-              </span>
-            </a>
-            <Link to="/contact" className={classes.link}>
-              <Button className={classes.button}>Contact</Button>
-            </Link>
-            <Link to="/projects" className={classes.link}>
-              <Button
-                color="primary"
-                variant="outlined"
-                className={classes.button}
+          <Button
+            aria-controls='simple-menu'
+            aria-haspopup='true'
+            onClick={handleClick}
+          >
+            <MenuIcon color='primary' fontSize='large' />
+          </Button>
+          <Menu
+            id='simple-menu'
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>
+              {" "}
+              <Link to='/about' className={classes.link}>
+                <Button className={classes.button}>About</Button>
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <a
+                href='https://resume.creddle.io/resume/bbjuttd019o'
+                className={classes.link}
+                target='_blank'
+                rel='noopener noreferrer'
               >
-                Projects
-              </Button>
-            </Link>
-          </div>
+                <span>
+                  <Button className={classes.button}>Resume</Button>
+                </span>
+              </a>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link to='/contact' className={classes.link}>
+                <Button className={classes.button}>Contact</Button>
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link to='/projects' className={classes.link}>
+                <Button
+                  color='primary'
+                  variant='outlined'
+                  className={classes.button}
+                >
+                  Projects
+                </Button>
+              </Link>
+            </MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
     </>
